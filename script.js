5433057fa7d9c298dbcebe482f0ab6edcd3fe5d3
avigail-c <avigail.2012.calderon@gmail.com>
2026-06-29 13:50:12 +0300

    Refactor getInput function and add game board setup

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-function getInput(){
-    let userInput = prompt("please enter a number");
-    for(let i = 0; i<=userInput; i++){
-        if(i % 2 ==0){
-            document.writeln("<div class =header>"+ i+ "</div>")
-        }
-        x = Math.sqrt();
+const grid = document.getElementById('grid');
+const scoreDisplay = document.getElementById('score');
+let squares = 1;
+let currentSnake = [2, 1, 0];
+let direction = 1;
+let appleIndex = 0;
+let score = 0;
+let timerId = 0;
+let intervalTime = 200;
+
+function createBoard() {
+    for (let i = 0; i < 400; i++){
+        const square = document.createElement('div');
+        grid.appendChild(square);
+        squares.push(square);
     }
-    alert("done!");
-
 }
-
-
-
-
-
-getInput();
+createBoard();

42f05eafdb81acff98b42166b0f57c58b844bc1a
avigail-c <avigail.2012.calderon@gmail.com>
2026-06-29 12:22:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function getInput(){
+    let userInput = prompt("please enter a number");
+    for(let i = 0; i<=userInput; i++){
+        if(i % 2 ==0){
+            document.writeln("<div class =header>"+ i+ "</div>")
+        }
+        x = Math.sqrt();
+    }
+    alert("done!");
+
+}
+
+
+
+
+
+getInput();
